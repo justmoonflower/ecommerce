ed64f18e201c28464d5b7379e0470e7b33d38c88
moonflower <tayler.brianne.baldwin@gmail.com>
2024-03-27 15:04:32 -0400

    category: routes and model

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,7 +7,22 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    // id is an integer datatype with flase allowNull, true primaryKey and true autoIncrement
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    
+    // category_name is a string datatype with false allowNull
+    
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
+
   {
     sequelize,
     timestamps: false,
