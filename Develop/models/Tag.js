2f8ab713ec4368ae7512ea35de62bc411e73f29a
moonflower <tayler.brianne.baldwin@gmail.com>
2024-03-27 15:25:27 -0400

    tag added was not included

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -5,9 +5,22 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
+
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    tag_name: {
+      type: DataTypes.STRING,
+    }
+
   },
+
   {
     sequelize,
     timestamps: false,
@@ -15,6 +28,7 @@ Tag.init(
     underscored: true,
     modelName: 'tag',
   }
+  
 );
 
 module.exports = Tag;
