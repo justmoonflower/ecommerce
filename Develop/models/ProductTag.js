a329f303c3fe1dbfcdb4e47f02b1c1bbbafe091a
moonflower <tayler.brianne.baldwin@gmail.com>
2024-03-27 15:19:55 -0400

    product routes and product tag routes

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -5,8 +5,31 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
+
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement:true
+    },
+
+    product_id: {
+      type: DataTypes.INTEGER,
+      refrences: {
+        model: 'product',
+        key:'id'
+        }
+    },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      refrences: {
+        model: 'tag',
+        key:'id'
+        }
+    }
   },
   {
     sequelize,
@@ -15,6 +38,7 @@ ProductTag.init(
     underscored: true,
     modelName: 'product_tag',
   }
+  
 );
 
 module.exports = ProductTag;
